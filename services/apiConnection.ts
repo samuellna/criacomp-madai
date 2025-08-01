84bfe069dae340ca8f75c9338100574bbb458fa3
samuellna <samuelnunes4039@gmail.com>
2025-07-31 21:49:10 -0300

    fix(transcription): remove content-type from headers

--- services/apiConnection.ts
+++ services/apiConnection.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.openai.com/v1",
   headers: {
-    "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
   },
 });
